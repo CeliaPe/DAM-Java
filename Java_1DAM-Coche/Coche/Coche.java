cc65dae8e7ae58cbcb28f39bf782b5ece94b2553
Celia <celia.pedregosa@itgranada.com>
2022-03-14 10:33:36 +0100

    abrir cualquier ventana/puerta

--- Java_1DAM-Coche/Coche/Coche.java
+++ Java_1DAM-Coche/Coche/Coche.java
@@ -20,6 +20,24 @@ class Coche{
 	public void abrirVentanaDer(){
 		this.puertaDer.getVentana().abrir();
 	}
+	//Para abrir cualquier puerta/ventana:
+	public void abrirCualquierPuerta(int puerta){
+		if (puerta==1){
+			this.puertaIzq.abrir()
+		}
+		if (puerta==2){
+			this.puertaDer.abrir()
+		}
+	}
+	//Asumo que abrirVentanaDer() no existe
+	public void abrirCualquierVentana(int ventana){
+		if (ventana==1){
+			this.puertaIzq.getVentana().abrir();
+		}
+		else if (ventana==2){
+			this.puertaDer.getVentana().abrir();
+		}
+	}
 	public String toString(){
 		return this.motorCoche + "\n" + this.puertaDer + "\n" + this.puertaIzq;
 	}
