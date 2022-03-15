962a030c7d950671746f939aa47e11916dbc62ff
Celia <celia.pedregosa@itgranada.com>
2022-03-15 13:17:53 +0100

    Explicación herencia

--- Persona/Persona.java
+++ Persona/Persona.java
@@ -1,1 +1,6 @@
 // Clase persona Herencia
+// Dos tipos de herencia: extension (a los métodos del padre le añadimos métodos
+// propios) y especializacion (modificamos los métodos del padre para adaptarlos
+// a nuestras necesidades).
+// Además de la herencia por agregación o composición (añadir clases como atri-
+// butos de nuestra clase) explicado en coche.
