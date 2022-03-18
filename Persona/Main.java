e39cc2de8e2caf52ab20dc0c968ea1c1047f4d81
Celia <celia.pedregosa@itgranada.com>
2022-03-18 13:09:06 +0100

    Creamos matriz

--- Persona/Main.java
+++ Persona/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Main {
   public static void main(String[] args){
     Persona miPersona = new Persona();
@@ -5,6 +7,17 @@ class Main {
     Alumno miAlumno = new Alumno("Marivi","98765432H","ASD");
     //System.out.println(miPersona);
     System.out.println(miAlumno);
+
+    ArrayList<Integer> lista = new ArrayList();
+    lista.add(4);
+    lista.add(5);
+    System.out.println(lista);
+
+    ArrayList<Alumno> listaAlumno = new ArrayList();
+    listaAlumno.add(miAlumno);
+    listaAlumno.add(alumnoDefecto);
+    System.out.println(listaAlumno);
+    System.out.println(listaAlumno.get(0).getNombre());
 
   }
 }
