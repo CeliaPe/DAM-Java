f1c17b79af4082a3b6f4046900cbac589d1d97db
Celia <celia.pedregosa@itgranada.com>
2022-05-10 13:25:53 +0200

    Switch case...

--- Persona/Alumno.java
+++ Persona/Alumno.java
@@ -1,21 +1,70 @@
 public class Alumno extends Persona{
+  private enum Nota{
+    SOBRESALIENTE,
+    NOTABLE,
+    BIEN,
+    SUFICIENTE,
+    INSUFICIENTE
+  }
+  // Como traduce Java un ENUM:
+  /*
+  class Nota{
+    public static final Nota SOBRESALIENTES = new Nota();
+    public static final Nota NOTABLE = new Nota();
+    public static final Nota BIEN = new Nota();
+    .
+    .
+  }
+  */
+
   private   String grado;               // Acceso desde la propia clase.
             String gradoSinModificador; // Acceso desde el paquete.
   protected String gradoProtegido;      // Acceso desde hijos y desde el paquete.
   public    String gradoPublico;        // Acceso global.
 
+  private   Nota   nota;
+  private   String resultadoNotas;      // No tiene set/get proque se utiliza solo en el switch.
 
   public Alumno(){
     super();
     this.grado = "DAM";
+    this.nota = Nota.SUFICIENTE;
   }
   public Alumno(String nombre, String dni, String grado){
     // Super llama al constructor de la clase superior (de la que extendemos).
     super(nombre,dni);
     this.grado = grado;
+    this.nota = Nota.SUFICIENTE;
+  }
+  public Alumno(Alumno alumno){
+    super (alumno.nombre,alumno.dni);
+    this.grado = alumno.grado;
+    this.nota = alumno.nota;
+  }
+
+  private String stringNotas(){
+    switch(this.nota){
+      case SOBRESALIENTE:
+        resultadoNotas = "Oleeeeee! Así se hase! :)";
+        break;
+      case NOTABLE:
+        resultadoNotas = "Lo has hecho muy bien!";
+        break;
+      case BIEN:
+        resultadoNotas = "Bien!";
+        break;
+      case SUFICIENTE:
+        resultadoNotas = "Ta' bien... Pero por lo' peloh";
+        break;
+      case INSUFICIENTE:
+        resultadoNotas = "Así NO se hase! :(";
+        break;
+    }
+    return resultadoNotas;
   }
 
   public String toString(){
-    return super.toString() + " Estoy en el grado de " + this.grado + ".";
+    return super.toString() + " Estoy en el grado de " + this.grado +
+    " y el profesor me ha dicho que " + this.stringNotas();
   }
 }
--- Persona/Main.java
+++ Persona/Main.java
@@ -4,10 +4,10 @@ class Main {
   public static void main(String[] args){
     Persona miPersona = new Persona();
     Alumno alumnoDefecto = new Alumno();
-    Alumno miAlumno = new Alumno("Marivi","98765432H","ASD");
+    Alumno miAlumno = new Alumno("Celia","123123123O","DAM");
     //System.out.println(miPersona);
     System.out.println(miAlumno);
-
+    /*
     ArrayList<Integer> lista = new ArrayList();
     lista.add(4);
     lista.add(5);
@@ -18,5 +18,6 @@ class Main {
     listaAlumno.add(alumnoDefecto);
     System.out.println(listaAlumno);
     System.out.println(listaAlumno.get(0).getNombre());
+    */
   }
 }
