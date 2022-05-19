e9b4522cf6f55f0e18c87c1e0fcca808999c04cf
Celia <celia.pedregosa@itgranada.com>
2022-05-19 12:19:57 +0200

    Ejemplo try-catch

--- Albergue/Main.java
+++ Albergue/Main.java
@@ -29,25 +29,32 @@ public class Main{
 
     //System.out.println("-- Comprobar que el tipo de habitacion es valido --");
     while (!valido){
-      System.out.println("-- Solicitar tipo de habitacion, numero de personas y numero de noches --");
-      System.out.print("Indica el tipo de habitacion que desea: ");
-      tipoHabit = sc.nextLine();
-      habitacion = Albergue.valueOf(tipoHabit);
-      System.out.print("Indica el numero de personas que sois: ");
-      //numPersonas = sc.nextInt(); sc.nextLine();
-      numPersonas = Integer.parseInt(sc.nextLine());
-      valido = habitacion.numPersValido(numPersonas);
+      try{
+        System.out.println();
+        // System.out.println("-- Solicitar tipo de habitacion, numero de personas y numero de noches --");
+        System.out.print("Indica el tipo de habitacion que desea: ");
+        tipoHabit = sc.nextLine();
+        habitacion = Albergue.valueOf(tipoHabit);
+        System.out.print("Indica el numero de personas que sois: ");
+        //numPersonas = sc.nextInt(); sc.nextLine();
+        numPersonas = Integer.parseInt(sc.nextLine());
+        valido = habitacion.numPersValido(numPersonas);
+      } catch (Exception e){
+        System.out.println("Estoy en el catch");
+      } finally {
+        System.out.println("Estoy en el finally");
+      }
     }
     System.out.print("Indica el numero de noches que os vais a quedar: ");
     //numNoches = sc.nextInt(); sc.nextLine();
     numNoches = Integer.parseInt(sc.nextLine());
-    System.out.println("-- Calcular precio total -- ");
+    //System.out.println("-- Calcular precio total -- ");
     precioTotal = habitacion.precioTotal(numNoches);
     System.out.println("El precio total es de: " + precioTotal);
-    System.out.println("-- Calcular precio por persona -- ");
+    //System.out.println("-- Calcular precio por persona -- ");
     precioPorPersona = habitacion.precioPorPersona(precioTotal,numPersonas);
     System.out.println("El precio por persona es de: " + precioPorPersona);
-    System.out.println("-- Proponer habitacion alternativa con descuento --");
+    //System.out.println("-- Proponer habitacion alternativa con descuento --");
     if (habitacion.getHabitaciones() == 0){
       System.out.println("Hemos cometido un error y no quedan libres habitaciones como las reservada, tenemos estas opciones con descuento: ");
       solucionAlternativa(numPersonas);
